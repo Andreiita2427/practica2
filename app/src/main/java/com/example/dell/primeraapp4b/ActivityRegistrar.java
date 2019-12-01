16cd9560fdc850c26d2e473668052382924b1bc0
Andreiita Alcívar <andrea.rocio6.24@gmail.com>
2019-11-30 23:17:36 -0500

    practica2

---
+++ app/src/main/java/com/example/dell/primeraapp4b/ActivityRegistrar.java
@@ -0,0 +1,13 @@
+package com.example.dell.primeraapp4b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityRegistrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
---
+++ app/src/main/java/com/example/dell/primeraapp4b/ParametroPasar.java
@@ -0,0 +1,33 @@
+package com.example.dell.primeraapp4b;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ParametroPasar extends AppCompatActivity {
+
+    EditText cajaDato;
+    Button botonEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametro_pasar);
+        cajaDato = (EditText) findViewById(R.id.txtParametro);
+        botonEnviar = (Button) findViewById(R.id.btnEnviarParametro);
+        botonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ParametroPasar.this, ParametroRecibir.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("dato", cajaDato.getText().toString());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
